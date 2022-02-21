e649a62ee416e8b7291c58a2ed8009cbd6a97142
dylpc <1079108132@qq.com>
2022-02-21 16:26:53 +0800

    2022.2.19 增加了等待时间 服务表增加了

--- run_server.py
+++ run_server.py
@@ -10,8 +10,8 @@ if __name__ == '__main__':
     mysql_sql.USER_INFO_insert("18811891816", "18811891816")
     # SERVICE_INFO测试表
     mysql_sql.SERVICE_INFO_insert("Service1", "trainable model ", "Service1 with trained model", True, False , "wuchh25")
-    mysql_sql.SERVICE_INFO_insert("Service2", "trainable model ", "Service2 with trained model", False, True, "wuchh25")
-    mysql_sql.SERVICE_INFO_insert("Service3", "trainable model ", "Service3 with trained model", False, False, "wuchh25")
+    mysql_sql.SERVICE_INFO_insert("Service2", "trainable model ", "Service2 with trained model", False, True, "XXXXX")
+    mysql_sql.SERVICE_INFO_insert("Service3", "trainable model ", "Service3 with trained model", False, False, "wuchh")
     # GLOBAL_MODEL_INFO测试表
     mysql_sql.GLOBAL_MODEL_INFO_insert("Service1", "v1", "aaa", "2022-02-18 00:00:00", 1, "2022-02-19 00:00:00",
                                        "model_path")
